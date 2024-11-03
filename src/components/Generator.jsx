7e6765aad4db39205b5cb4327b00e1b74ddb8552
sangeeth-606 <sangeeth999123@gmail.com>
2024-11-03 17:02:36 +0530

    Move project files to root directory

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Sectionwrapper from "./Sectionwrapper";
 import { SCHEMES, WORKOUTS } from "../utils/swoldier";
 
@@ -19,12 +19,10 @@ function Header(props) {
 }
 
 export default function Generator() {
-
-  const  [showModel, setShowModel] = useState(false); 
-  const  [poison, setPoison] = useState('individual');
+  const [showModel, setShowModel] = useState(false);
+  const [poison, setPoison] = useState("individual");
   const [muscles, setMuscles] = useState([]);
-  const [goal, setGoal] = useState('strength_power');
-  
+  const [goal, setGoal] = useState("strength_power");
 
   function toggleModal() {
     setShowModel(!showModel);
@@ -43,17 +41,22 @@ export default function Generator() {
 
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
         {Object.keys(WORKOUTS).map((type, typeIndex) => {
-            return (
-            <button onClick={() => {setPoison(type)}}
-              
-              className={"bg-slate-950 border border-blue-400 py-3 rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-lg" + (poison === type ? ' border-blue-900' : '')}
+          return (
+            <button
+              onClick={() => {
+                setPoison(type);
+              }}
+              className={
+                "bg-slate-950 border border-blue-400 py-3 rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-lg" +
+                (poison === type ? " border-blue-900" : "")
+              }
               key={typeIndex}
             >
               <p className=" capitalize text-white">
-              {type.replaceAll("_", " ")}
+                {type.replaceAll("_", " ")}
               </p>
             </button>
-            );
+          );
         })}
       </div>
       {/* //----------------------------------- */}
@@ -73,7 +76,6 @@ export default function Generator() {
         </button>
         {showModel && <div>modal</div>}
       </div>
-      
 
       <Header
         index={"03"}
@@ -84,13 +86,18 @@ export default function Generator() {
       <div className="grid grid-cols-3  gap-4">
         {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
           return (
-            <button onClick={() => {setGoal(scheme)}}
-              
-              className={"bg-slate-950 border border-blue-400 py-3 rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-lg" + (scheme === goal ? ' border-blue-900' : '')}
+            <button
+              onClick={() => {
+                setGoal(scheme);
+              }}
+              className={
+                "bg-slate-950 border border-blue-400 py-3 rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-lg" +
+                (scheme === goal ? " border-blue-900" : "")
+              }
               key={schemeIndex}
             >
               <p className=" capitalize text-white">
-              {scheme.replaceAll("_", " ")}
+                {scheme.replaceAll("_", " ")}
               </p>
             </button>
           );
